2d529de20d7256e7d1dd17040adea9302c940faa
Nikil47 <77465336+Nikil47@users.noreply.github.com>
2022-08-15 15:32:23 +0200

    Removing CORS and build executable

--- main.go
+++ main.go
@@ -29,10 +29,6 @@ func main() {
 		}
 		go parseGameDeals(ch, maxPrice, minPrice, gameTitle)
 		var result []gameDeal = <-ch
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 		c.JSON(http.StatusOK, result)
 	})
 	r.Run()
